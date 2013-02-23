9a5643b8a3e73ee2eb17c3b51a19397b8e5814ca
Lars Christian Jensen <larschri@pvv.ntnu.no>
2013-02-23 15:27:47 -0800

    minimize/add missing types

--- src/org/pvv/larschri/gameday/xml/Game.java
+++ src/org/pvv/larschri/gameday/xml/Game.java
@@ -1,5 +1,6 @@
 package org.pvv.larschri.gameday.xml;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -17,7 +18,7 @@ import javax.xml.datatype.XMLGregorianCalendar;
  *  <li>Download http://gd2.mlb.com/components/game/mlb/year_2012/month_06/day_13/gid_2012_06_13_houmlb_sfnmlb_1/inning/inning_all.xml</li>
  *  <li>Create xsd on: http://xmlgrid.net/xml2xsd.html</li>
  *  <li>Generate java: using xjc ~/Downloads/game.xsd -p org.pvv.larschri.gameday.xml -d src</li>
- *  <li>Manually make it work, remove verbose and redundant parts.
+ *  <li>Manually make it work, remove verbose and redundant parts and add types that the tools decided to skip.
  * </ul>
  */
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -25,22 +26,12 @@ import javax.xml.datatype.XMLGregorianCalendar;
 @XmlRootElement(name = "game")
 public class Game {
 
-	@XmlElement(required = true) protected List<Game.Inning> inning;
+	@XmlElement(required = true) protected List<Game.Inning> inning = new ArrayList<>();
 	@XmlAttribute protected Integer atBat;
 	@XmlAttribute protected Integer deck;
 	@XmlAttribute protected Integer hole;
 	@XmlAttribute protected String ind;
 
-	public List<Game.Inning> getInning() { return this.inning; }
-
-	public Integer getAtBat() { return atBat; }
-	public Integer getDeck() { return deck; }
-	public Integer getHole() { return hole; }
-	public String getInd() { return ind; }
-
-
-	@XmlAccessorType(XmlAccessType.FIELD)
-	@XmlType(name = "", propOrder = { "top", "bottom" })
 	public static class Inning {
 
 		@XmlElement(required = true) protected Game.Inning.HalfInning top;
@@ -50,25 +41,26 @@ public class Game {
 		@XmlAttribute(name = "home_team") protected String homeTeam;
 		@XmlAttribute protected String next;
 
-		public Game.Inning.HalfInning getTop() { return top; }
-		public Game.Inning.HalfInning getBottom() { return bottom; }
-		public Integer getNum() { return num; }
-		public String getAwayTeam() { return awayTeam; }
-		public String getNext() { return next; }
-
-		@XmlAccessorType(XmlAccessType.FIELD)
-		@XmlType(name = "", propOrder = { "atbat" })
 		public static class HalfInning {
 
-			@XmlElement(required = true) protected List<Game.Inning.HalfInning.Atbat> atbat;
+			@XmlElement(required = true) protected List<Game.Inning.HalfInning.Atbat> atbat = new ArrayList<>();
+			@XmlElement(required = false) protected List<Game.Inning.HalfInning.Action> action = new ArrayList<>();
 
-			public List<Game.Inning.HalfInning.Atbat> getAtbat() { return this.atbat; }
+			public static class Action {
+				@XmlAttribute protected Integer b;
+				@XmlAttribute protected Integer s;
+				@XmlAttribute protected Integer o;
+				@XmlAttribute protected String des;
+				@XmlAttribute protected String event;
+				@XmlAttribute protected Integer player;
+				@XmlAttribute protected Integer pitch;
+				@XmlAttribute(name = "tfs_zulu") @XmlSchemaType(name = "dateTime") protected XMLGregorianCalendar tfsZulu;
+			}
 
-			@XmlAccessorType(XmlAccessType.FIELD)
-			@XmlType(name = "", propOrder = { "pitch" })
 			public static class Atbat {
 
-				@XmlElement(required = true) protected List<Game.Inning.HalfInning.Atbat.Pitch> pitch;
+				@XmlElement(required = true) protected List<Game.Inning.HalfInning.Atbat.Pitch> pitch = new ArrayList<>();
+				@XmlElement(required = false) protected List<Game.Inning.HalfInning.Atbat.Runner> runner = new ArrayList<>();
 				@XmlAttribute protected Integer num;
 				@XmlAttribute protected Integer b;
 				@XmlAttribute protected Integer s;
@@ -83,22 +75,16 @@ public class Game {
 				@XmlAttribute protected String des;
 				@XmlAttribute protected String event;
 
-				public List<Game.Inning.HalfInning.Atbat.Pitch> getPitch() { return this.pitch; }
-				public Integer getNum() { return num; }
-				public Integer getB() { return b; }
-				public Integer getS() { return s; }
-				public Integer getO() { return o; }
-				public Integer getStartTfs() { return startTfs; }
-				public XMLGregorianCalendar getStartTfsZulu() { return startTfsZulu; }
-				public Integer getBatter() { return batter; }
-				public String getStand() { return stand; }
-				public String getBHeight() { return bHeight; }
-				public Integer getPitcher() { return pitcher; }
-				public String getPThrows() { return pThrows; }
-				public String getDes() { return des; }
-				public String getEvent() { return event; }
+				public static class Runner {
+					@XmlAttribute protected Integer id;
+					@XmlAttribute protected String start;
+					@XmlAttribute protected String end;
+					@XmlAttribute protected String event;
+					@XmlAttribute protected String score;
+					@XmlAttribute protected String rbi;
+					@XmlAttribute protected String earned;
+				}
 
-				@XmlAccessorType(XmlAccessType.FIELD) @XmlType(name = "")
 				public static class Pitch {
 					@XmlAttribute protected String des;
 					@XmlAttribute protected Integer id;
@@ -136,43 +122,6 @@ public class Game {
 					@XmlAttribute(name = "spin_rate") protected Double spinRate;
 					@XmlAttribute protected String cc;
 					@XmlAttribute protected String mt;
-
-					public String getDes() { return des; }
-					public Integer getId() { return id; }
-					public String getType() { return type; }
-					public Integer getTfs() { return tfs; }
-					public XMLGregorianCalendar getTfsZulu() { return tfsZulu; }
-					public Double getX() { return x; }
-					public Double getY() { return y; }
-					public String getSvId() { return svId; }
-					public Double getStartSpeed() { return startSpeed; }
-					public Double getEndSpeed() { return endSpeed; }
-					public Double getSzTop() { return szTop; }
-					public Double getSzBot() { return szBot; }
-					public Double getPfxX() { return pfxX; }
-					public Double getPfxZ() { return pfxZ; }
-					public Double getPx() { return px; }
-					public Double getPz() { return pz; }
-					public Double getX0() { return x0; }
-					public Double getY0() { return y0; }
-					public Double getZ0() { return z0; }
-					public Double getVx0() { return vx0; }
-					public Double getVy0() { return vy0; }
-					public Double getVz0() { return vz0; }
-					public Double getAx() { return ax; }
-					public Double getAy() { return ay; }
-					public Double getAz() { return az; }
-					public Double getBreakY() { return breakY; }
-					public Double getBreakAngle() { return breakAngle; }
-					public Double getBreakLength() { return breakLength; }
-					public String getPitchType() { return pitchType; }
-					public Double getTypeConfidence() { return typeConfidence; }
-					public Integer getZone() { return zone; }
-					public Integer getNasty() { return nasty; }
-					public Double getSpinDir() { return spinDir; }
-					public Double getSpinRate() { return spinRate; }
-					public String getCc() { return cc; }
-					public String getMt() { return mt; }
 				}
 			}
 		}
